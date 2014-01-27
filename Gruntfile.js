472f81852e08a7c2b0394c348be2b235fa071b03
Juho Salmio <juho.salmio@gmail.com>
2014-01-27 16:20:56 +0200

    added grunt jshint task

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: ['Gruntfile.js', 'payment.js']
+    }
+
+  });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+
+};
