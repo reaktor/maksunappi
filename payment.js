0beb314a3d0009d479ff451c80fe69c00af54f8e
Laura Leppänen <laura.leppanen@reaktor.fi>
2014-01-27 14:30:58 +0200

    Module skeleton.

---
+++ payment.js
@@ -0,0 +1,14 @@
+var crypto = require("crypto")
+  , events = require('events')
+  , _ = require('underscore')._
+  , express = require('express');
+
+exports.create = function (config) {
+  var paymentGen = Object.create(events.EventEmitter.prototype);
+
+  paymentGen.paymentButton = function (options) {
+    return "";
+  };
+
+  return paymentGen;
+};
