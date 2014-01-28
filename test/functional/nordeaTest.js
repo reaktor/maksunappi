5a2211aac94c168bf7b7844777c8c2302e76d0e1
Juho Salmio <juho.salmio@gmail.com>
2014-01-28 17:41:00 +0200

    add functional tests for nordea

---
+++ test/functional/nordeaTest.js
@@ -0,0 +1,59 @@
+var config = require("../../config.json");
+var x = require('casper').selectXPath;
+
+casper.test.begin("Nordea Payment", 1, function(test) {
+  casper.start('https://localhost:' + config.port, function() {
+    this.click("#nordea-payment");
+  });
+
+  casper.then(function() {
+    this.fill('form[name="A232202Y"]', {
+      'A02Y_USERID': '123456',
+      'A02Y_IDNBR': '1111'
+    }, true);
+  });
+
+  casper.then(function() {
+    this.fill('form[name="A232203Y"]', {
+      'A03Y_PMTCONNB': '1111'
+    }, true);
+  });
+
+  casper.then(function() {
+    this.click("input[type='button']");
+  });
+  casper.then(function(){
+    test.assertExists("#success");
+    this.echo("Succesfully paid with Nordea");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("Nordea Payment Cancel", 1, function(test) {
+
+  casper.start('https://localhost:' + config.port, function() {
+    this.click("#nordea-payment");
+  });
+
+  casper.then(function() {
+    this.click("input[name='A02Y_DELEBTN']");
+  });
+
+  casper.then(function() {
+    //TODO get commented code to work
+    //this.click(x("//a[.='Paluu myyjälle']"));
+    this.click(x("/html/body/table/tbody/tr[2]/td/div/div/p[2]/a"));
+  });
+
+  casper.then(function(){
+    test.assertExists("#cancel");
+    this.echo("Succesfully canceled payment with Nordea");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
