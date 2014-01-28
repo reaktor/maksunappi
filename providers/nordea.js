a816341a042c4bb45900952502d177fb3a931b46
Laura Leppänen <laura.leppanen@reaktor.fi>
2014-01-28 12:43:53 +0200

    Format date (Nordea).

--- providers/nordea.js
+++ providers/nordea.js
@@ -9,15 +9,15 @@ exports.mapParams = function (providerConfig, options) {
     SOLOPMT_VERSION: providerConfig.paymentVersion,
     SOLOPMT_STAMP: options.requestId,
     SOLOPMT_RCV_ID: providerConfig.vendorId,
-    SOLOPMT_LANGUAGE: mapLanguage(options.language),
+    SOLOPMT_LANGUAGE: formatLanguage(options.language),
     SOLOPMT_AMOUNT: formatAmount(options.amount),
     SOLOPMT_REF: options.paymentReference,
-    SOLOPMT_DATE: 'EXPRESS', // TODO: specify date in options, define default in config
+    SOLOPMT_DATE: formatDueDate(options.dueDate, providerConfig.dueDate),
     SOLOPMT_MSG: options.message, // TODO: formatting
     SOLOPMT_RETURN: providerConfig.returnUrls.ok,
     SOLOPMT_CANCEL: providerConfig.returnUrls.cancel,
     SOLOPMT_REJECT: providerConfig.returnUrls.reject,
-    SOLOPMT_CONFIRM: confirmation(options.confirm, providerConfig.confirm),
+    SOLOPMT_CONFIRM: formatConfirmation(options.confirm, providerConfig.confirm),
     SOLOPMT_KEYVERS: providerConfig.keyVersion,
     SOLOPMT_CUR: providerConfig.currency,
     SOLOPMT_PMTTYPE: options.mobile ? 'M' : undefined,
@@ -27,12 +27,17 @@ exports.mapParams = function (providerConfig, options) {
   };
 };
 
-function confirmation(confirm, defaultValue) {
-  if (_.isNull(confirm) || _.isUndefined(confirm)) {
-    return defaultValue;
-  } else {
-    return toParam(confirm);
+function formatDueDate(date, defaultValue) {
+  return formatOrDefault(date, toParam, defaultValue);
+
+  function toParam(date) {
+    return _.str.sprintf("%02d.%02d.%4d",
+      date.getDate(), date.getMonth() + 1, date.getFullYear());
   }
+}
+
+function formatConfirmation(confirm, defaultValue) {
+  return formatOrDefault(confirm, toParam, defaultValue);
 
   function toParam(confirm) {
     if (confirm) {
@@ -43,11 +48,21 @@ function confirmation(confirm, defaultValue) {
   }
 }
 
+function formatOrDefault(param, formatToParam, defaultValue) {
+  if (_.isNull(param) || _.isUndefined(param)) {
+    return defaultValue;
+  } else {
+    return formatToParam(param);
+  }
+}
+
 function formatAmount(amount) {
   return _.str.numberFormat(amount, 2, ',', '')
 }
 
-function mapLanguage(langCode) {
+function formatLanguage(langCode) {
+  if (!langCode) return 1;
+
   switch (langCode.toUpperCase()) {
     case "FI": return 1;
     case "SV": return 2;
