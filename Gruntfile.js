d7bd62999169f4c17d4bbc27aaf92e0b3f2fb7c3
Juho Salmio <juho.salmio@gmail.com>
2014-01-28 13:51:43 +0200

    added mocha for unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,22 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
-      all: ['Gruntfile.js', 'app.js', 'payment.js', 'providers/*']
+      all: ['Gruntfile.js', 'app.js', 'payment.js', 'providers/*', 'test/unit/*.js']
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/unit/*.js']
+      }
     }
 
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
