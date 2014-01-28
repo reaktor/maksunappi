37fd80eb1a6ef80f423cbfc4a9950d889dbc4b06
Juho Salmio <juho.salmio@gmail.com>
2014-01-28 10:51:46 +0200

    added aktia and alandsbanken mappings

--- providers/aktia.js
+++ providers/aktia.js
@@ -1,5 +1,17 @@
-// Provider stub
-
 exports.mapParams = function (providerConfig, options) {
-  return {};
+  return {
+    "NET_VERSION" : providerConfig.paymentVersion,
+    "NET_STAMP" : options.requestId,
+    "NET_SELLER_ID" : providerConfig.vendorId,
+    "NET_AMOUNT" : options.amount,
+    "NET_CUR" : providerConfig.currency,
+    "NET_REF" : options.paymentReference,
+    "NET_DATE" : options.dueDate,
+    "NET_MSG" : options.message,
+    "NET_RETURN" : providerConfig.returnUrl,
+    "NET_CANCEL" : providerConfig.cancelUrl,
+    "NET_REJECT" : providerConfig.rejectUrl,
+    "NET_MAC" : options.checksum,
+    "NET_CONFIRM" : providerConfig.confirm
+  };
 };
--- providers/alandsbanken.js
+++ providers/alandsbanken.js
@@ -1,5 +1,23 @@
-// Provider stub
-
 exports.mapParams = function (providerConfig, options) {
-  return {};
+  return {
+    "AAB_VERSION" : providerConfig.paymentVersion,
+    "AAB_STAMP" : options.requestId,
+    "AAB_RCV_ID" : providerConfig.vendorId,
+    "AAB_RCV_ACCOUNT" : options.amount,
+    "AAB_RCV_NAME" : providerConfig.vendorName,
+    "AAB_LANGUAGE" : options.language,
+    "AAB_AMOUNT" : options.amount,
+    "AAB_REF" : options.paymentReference,
+    "AAB_DATE" : options.dueDate,
+    "AAB_MSG" : options.message,
+    "AAB_RETURN" : providerConfig.returnUrl,
+    "AAB_CANCEL" : providerConfig.cancelUrl,
+    "AAB_REJECT" : providerConfig.rejectUrl,
+    "AAB_MAC" : options.checksum,
+    "AAB_CONFIRM" : providerConfig.confirm,
+    "AAB_KEYVERS" : providerConfig.keyVersion,
+    "AAB_CUR" : providerConfig.currency,
+    "BC_UseBVCookie" : providerConfig.cookie
+
+  };
 };
