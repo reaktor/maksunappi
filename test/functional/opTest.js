b7b95a60c01611beb3f3db1724afec2ba58077df
Juho Salmio <juho.salmio@gmail.com>
2014-01-31 10:06:58 +0200

    add functional test for op

---
+++ test/functional/opTest.js
@@ -0,0 +1,59 @@
+var config = require("../../config.json");
+var x = require('casper').selectXPath;
+
+casper.test.begin("OP Payment", 1, function(test) {
+  casper.start('https://localhost:' + config.port, function() {
+    this.click("#op-payment");
+  });
+
+  casper.then(function() {
+    this.fill('form[name="lomake"]', {
+      'id': '123456',
+      'pw': '7890'
+    }, true);
+  });
+
+  casper.then(function() {
+    this.fill('form[name="lomake"]', {
+      'avainluku': '1111'
+    }, true);
+  });
+
+  casper.then(function() {
+    this.click('input[id="Toiminto"]');
+  });
+
+  casper.waitForSelector('#success', function() {
+    test.assertExists("#success");
+    this.echo("Succesfully authenticated with OP");
+  }, function(){}, 10000);
+
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("OP Payment Cancel", 1, function(test) {
+
+  casper.start('https://localhost:' + config.port, function() {
+    this.click("#op-payment");
+  });
+
+  casper.then(function() {
+    this.click("a[name='Lopeta']");
+  });
+
+  casper.then(function() {
+    this.click(x("//a[contains(text(), '> Palaa palveluntarjoajan sivulle')]"));
+  });
+
+  casper.then(function(){
+    test.assertExists("#cancel");
+    this.echo("Succesfully canceled payment with OP");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
