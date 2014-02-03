de50ed61e9fb8c01fccc5009500e084b6879bbaa
Juho Salmio <juho.salmio@gmail.com>
2014-02-03 15:28:14 +0200

    add functional test for alandsbanken

---
+++ test/functional/alandsbankenTest.js
@@ -0,0 +1,84 @@
+var config = require("../../../config.json");
+var helpers = require('../../../helpers');
+
+require = patchRequire(require);
+var x = require('casper').selectXPath;
+
+var options = {
+  bankName: 'Alandsbanken',
+  id: 'alandsbanken',
+  username: '12345678',
+  password: '9999',
+  securityCode: '9999',
+  returnLinkText: 'Takaisin myyjän sivuille'
+};
+
+var returnLinkSelector = "//a[contains(text(), '"+options.returnLinkText+"')]";
+var bankId = options.id || options.bankName.toLowerCase();
+
+casper.test.begin(options.bankName + " Payment", 1, function (test) {
+  casper.start('https://localhost:' + config.port, function() {
+    this.click("#"+bankId+"-payment");
+  });
+
+  casper.then(function() {
+    this.fill('form[name="loginForm"]', {
+      'username': options.username,
+      'password': options.password
+    }, true);
+  });
+
+  casper.waitForSelector('form[name="loginPinForm"]',function() {
+    this.fill('form[name="loginPinForm"]', {
+      'pinCode': options.securityCode
+    }, true);
+  });
+
+  casper.then(function() {
+    this.evaluate(function() {
+      document.querySelector('select').selectedIndex = 1;
+      return true;
+    });
+  });
+
+  casper.waitForSelector('form[name="instantPaymentForm"]', function() {
+    this.fill('form[name="instantPaymentForm"]', {
+      'pincode': options.securityCode
+    }, false);
+    this.click('button[type="submit"][name="btn_accept"]');
+  });
+
+  casper.waitForSelector(x(returnLinkSelector), function() {
+    this.click(x(returnLinkSelector));
+  });
+
+
+  casper.waitForSelector('#success', function () {
+    test.assertExists("#success");
+    this.echo("Succesfully paid with "+options.bankName);
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin(options.bankName+" Payment Cancel", 1, function (test) {
+
+  casper.start('https://localhost:' + config.port, function() {
+    this.click("#"+bankId+"-payment");
+  });
+
+  casper.then(function() {
+    this.click(x(returnLinkSelector));
+  });
+
+  casper.then(function(){
+    test.assertExists("#cancel");
+    this.echo("Succesfully canceled payment with "+options.bankName);
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
