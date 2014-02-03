2fbf89e4bd89128d96e690d1f0c0ffc881de8f08
Juho Salmio <juho.salmio@gmail.com>
2014-02-03 19:48:02 +0200

    add helper for international rf reference calculation

--- referenceConverter.js
+++ referenceConverter.js
@@ -17,9 +17,36 @@ exports.toFinnishPaymentReference = function(numericIdentifier) {
   return numStr + referenceCheckNumber(weightedSum).toString();
 };
 
+http://www.fkl.fi/teemasivut/sepa/tekninen_dokumentaatio/Dokumentit/RF-viitteen_kayttoonotto_Suomessa_1.12.2010.pdf
+exports.toRFReference = function(finnishReferenceNumber) {
+  var RFtoStr = "271500"
+  var refStr = finnishReferenceNumber.toString() + RFtoStr;
+  var checksumStr = rfCheckNumber(refStr);
+  return "RF" + checksumStr + finnishReferenceNumber.toString();
+}
+
+function rfCheckNumber(refStr) {
+  var remainder = modulo(refStr , 97);
+  var num = 98 - remainder;
+  return num < 10 ? "0" + parseInt(num) : num.toString();
+
+}
+
 function referenceCheckNumber(sum) {
   var inverse = 10 - sum % 10;
   return inverse == 10 ? 0 : inverse;
+}
+
+//to avoid integeroverflow
+function modulo(divident, divisor) {
+  var partLength = 10;
+
+  while (divident.length > partLength) {
+    var part = divident.substring(0, partLength);
+    divident = (part % divisor) +  divident.substring(partLength);
+  }
+
+  return divident % divisor;
 }
 
 function cycleUpto(collection, length) {
--- test/unit/referenceConverter.js
+++ test/unit/referenceConverter.js
@@ -3,10 +3,16 @@ var assert = require("assert");
 var converter = require("../../referenceConverter");
 
 describe('Payment reference converter', function(){
-  it('should convert series of numbers to valid referenceFormat', function(){
+  it('should convert series of numbers to valid finnish referenceFormat', function(){
     assert.equal(12345672, converter.toFinnishPaymentReference(1234567));
     assert.equal(13, converter.toFinnishPaymentReference(1));
     assert.equal(110, converter.toFinnishPaymentReference(11));
     assert.equal("2014012911041312349", converter.toFinnishPaymentReference("201401291104131234"));
+  });
+
+  it('should convert finnish reference to rf reference', function(){
+    assert.equal("RF332348236", converter.toRFReference(2348236));
+    assert.equal("RF07123912", converter.toRFReference(123912));
+    assert.equal("RF75201401291104131234", converter.toRFReference("201401291104131234"));
   });
 });
