9acbb9bd7af8621de916efacf200a757be812ff9
Matti Suur-Askola <matti.suuraskola@gmail.com>
2017-12-08 13:10:17 +0200

    extend spnakki provider to support missing parameter "AAB_ALG". https://github.com/reaktor/maksunappi/issues/11

--- providers/spankki.js
+++ providers/spankki.js
@@ -1,3 +1,19 @@
 var shared = require('./aab-shared');
+var parameters = require('../parameters');
+
+var algorithmKey = {
+  "md5": "01",
+  "sha256": "03",
+};
+
+var sharedMapParams = shared.mapParams;
 
 module.exports = shared;
+
+module.exports.mapParams = function (options) {
+  parameters.requireParams(options, ['algorithmType']);
+  
+  var params = sharedMapParams(options);
+  params.AAB_ALG = algorithmKey[options.algorithmType];
+  return params;
+};
